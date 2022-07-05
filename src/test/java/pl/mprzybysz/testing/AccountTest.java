579898bd7c8948c3c54b647e5537b36b4e5fd64a
mprzybysz <mprzybysz.dev@gmail.com>
2022-07-05 18:45:46 +0200

    add the first test

---
+++ src/test/java/pl/mprzybysz/testing/AccountTest.java
@@ -0,0 +1,14 @@
+package pl.mprzybysz.testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class AccountTest {
+
+    @Test
+     void myTest(){
+        Account newAccount = new Account();
+        assertFalse(newAccount.isActive(), "Check if new account is not active");
+    }
+}
