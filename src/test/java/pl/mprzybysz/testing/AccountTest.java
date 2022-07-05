7c234868a85840b0eef0bc5d8fc331cab48c6060
mprzybysz <mprzybysz.dev@gmail.com>
2022-07-05 19:00:25 +0200

    configure test run

--- src/test/java/pl/mprzybysz/testing/AccountTest.java
+++ src/test/java/pl/mprzybysz/testing/AccountTest.java
@@ -3,6 +3,7 @@ package pl.mprzybysz.testing;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AccountTest {
 
@@ -10,5 +11,12 @@ class AccountTest {
      void myTest(){
         Account newAccount = new Account();
         assertFalse(newAccount.isActive(), "Check if new account is not active");
+    }
+    @Test
+    void myTest2(){
+        Account newAccount = new Account();
+        assertFalse(newAccount.isActive());
+        newAccount.activate();
+        assertTrue(newAccount.isActive());
     }
 }
